bc0c55a12aa822e59fafc416b13b4b8e26b386e6
Kaarthik-S12 <kaarthik.s2023ece@sece.ac.in>
2026-03-29 10:53:02 +0530

    e

--- TBLFrontend/src/Pages/SignUp.jsx
+++ TBLFrontend/src/Pages/SignUp.jsx
@@ -1,22 +1,22 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import './SignUp.css'; // Import the CSS file
+import './SignUp.css'; 
 import NavBar from '../Components/NavBar';
-import axios from 'axios'; // For making HTTP requests
+import axios from 'axios'; 
 
 function SignUp() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
-  const [error, setError] = useState(''); // For error messages
-  const [success, setSuccess] = useState(''); // For success messages
+  const [error, setError] = useState(''); 
+  const [success, setSuccess] = useState(''); 
   const navigate = useNavigate();
 
-  // Handle form submission
+  
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    // Validate inputs
+   
     if (!email || !password || !confirmPassword) {
       setError('All fields are required!');
       return;
@@ -28,26 +28,23 @@ function SignUp() {
     }
 
     try {
-      // Send a POST request to the backend
       const response = await axios.post('http://localhost:8080/signup', {
         email,
         password,
       });
 
-      // Handle success
       setSuccess('User registered successfully!');
-      setError(''); // Clear any previous errors
+      setError('');
       setTimeout(() => {
-        navigate('/login'); // Redirect to login page after 2 seconds
+        navigate('/login'); 
       }, 2000);
     } catch (error) {
-      // Handle errors
       if (error.response && error.response.data) {
-        setError(error.response.data); // Display error message from backend
+        setError(error.response.data); 
       } else {
-        setError('An error occurred. Please try again.'); // Generic error message
+        setError('An error occurred. Please try again.');
       }
-      setSuccess(''); // Clear any previous success messages
+      setSuccess(''); 
     }
   };
 
@@ -55,19 +52,15 @@ function SignUp() {
     <>
       <NavBar />
       <div className="signup-container">
-        {/* Welcome Section (Left Side) */}
         <div className="welcome-section">
           <h1>Welcome to .....</h1>
           <p>Explore the world and Make It Simple</p>
         </div>
 
-        {/* Sign-Up Form (Right Side) */}
         <div className="form-section">
           <form onSubmit={handleSubmit}>
-            {/* Display error message */}
             {error && <div className="error-message">{error}</div>}
 
-            {/* Display success message */}
             {success && <div className="success-message">{success}</div>}
 
             <label>Email</label>

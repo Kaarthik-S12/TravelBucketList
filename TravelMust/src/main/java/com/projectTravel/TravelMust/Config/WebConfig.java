d8e3a896993f6c643689c3a0cb3749f880424f0b
Kaarthik-S12 <kaarthik.s2023ece@sece.ac.in>
2026-04-15 17:46:05 +0530

    simple check on cors

--- TravelMust/src/main/java/com/projectTravel/TravelMust/Config/WebConfig.java
+++ TravelMust/src/main/java/com/projectTravel/TravelMust/Config/WebConfig.java
@@ -4,10 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+//15-4-2026
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    //15-4-2026
+    
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") 
